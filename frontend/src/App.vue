ed63073a73c7dfd5477be6118037db03e7bb1187
DHoper <gxgemini777@gmail.com>
2023-07-06 15:46:36 +0800

    後端更新
    後端更新

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { reactive, ref } from 'vue';
+import { data } from './fakeData';
+import { Bar } from 'vue-chartjs';
+const getData = reactive(data);
+const  chartData =  ref({
+        labels: [ 'January', 'February', 'March' ],
+        datasets: [ { data: [40, 20, 12] } ]
+      });
+
+console.log(getData[0]);
+
+</script>
+
+<template>
+  <Bar id="my-chart-id" :options="true" :data="chartData" />
+  <li v-for="item in getData" :key="item.date">
+    {{ item.date }}:
+  <li v-for="i in item.weather">
+    {{ i.h }}
+    {{ i.t }}
+  </li>
+  </li>
+</template>
+
+
